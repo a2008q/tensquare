104cb657e2d99d65ab968f02508c6b2b849f68fe
a2008q <a2008q@sina.com>
2021-09-04 17:18:03 +0800

    完善热门企业列表、推荐职位列表、最新职位列表接口

--- tensquare_recruit/src/main/java/com/tensquare/recruit/dao/EnterpriseDao.java
+++ tensquare_recruit/src/main/java/com/tensquare/recruit/dao/EnterpriseDao.java
@@ -1,14 +1,22 @@
 package com.tensquare.recruit.dao;
 
+import com.tensquare.recruit.pojo.Enterprise;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import com.tensquare.recruit.pojo.Enterprise;
+import java.util.List;
+
 /**
  * enterprise数据访问接口
- * @author Administrator
  *
+ * @author Administrator
  */
-public interface EnterpriseDao extends JpaRepository<Enterprise,String>,JpaSpecificationExecutor<Enterprise>{
-	
+public interface EnterpriseDao extends JpaRepository<Enterprise, String>, JpaSpecificationExecutor<Enterprise> {
+    /**
+     * 根据热门状态获取企业列表
+     *
+     * @param ishot
+     * @return
+     */
+    List<Enterprise> findByIshot(String ishot);
 }
--- tensquare_recruit/src/main/java/com/tensquare/recruit/dao/RecruitDao.java
+++ tensquare_recruit/src/main/java/com/tensquare/recruit/dao/RecruitDao.java
@@ -1,14 +1,29 @@
 package com.tensquare.recruit.dao;
 
+import com.tensquare.recruit.pojo.Recruit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import com.tensquare.recruit.pojo.Recruit;
+import java.util.List;
+
 /**
  * recruit数据访问接口
- * @author Administrator
  *
+ * @author Administrator
  */
-public interface RecruitDao extends JpaRepository<Recruit,String>,JpaSpecificationExecutor<Recruit>{
-	
+public interface RecruitDao extends JpaRepository<Recruit, String>, JpaSpecificationExecutor<Recruit> {
+    /**
+     * 查询推荐职位列表(按创建日期降序排序)
+     *
+     * @return
+     */
+    List<Recruit> findTop4ByStateOrderByCreatetimeDesc(String state);
+
+    /**
+     * 最新职位列表
+     *
+     * @param state
+     * @return
+     */
+    List<Recruit> findTop12ByStateNotOrderByCreatetimeDesc(String state);
 }

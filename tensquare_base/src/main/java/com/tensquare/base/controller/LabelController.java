bf59064e4a83536a3f3c3fa5d6e85664270f13c2
a2008q <a2008q@sina.com>
2021-09-04 10:05:26 +0800

    完成base模块

---
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -0,0 +1,49 @@
+package com.tensquare.base.controller;
+
+import com.tensquare.base.entity.Label;
+import com.tensquare.base.service.LabelService;
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/label")
+public class LabelController {
+    @Autowired
+    private LabelService labelService;
+
+    /**
+     * 查询全部列表
+     *
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public Result findAll() {
+        return new Result(true, StatusCode.OK, "查询成功",
+                labelService.findAll());
+    }
+
+    @RequestMapping(value = "/{labelId}", method = RequestMethod.GET)
+    public Result findById(@PathVariable String labelId) {
+        return new Result(true, StatusCode.OK, "查询成功", labelService.findById(labelId));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Result add(@RequestBody Label label) {
+        labelService.add(label);
+        return new Result(true, StatusCode.OK, "增加成功");
+    }
+
+    @RequestMapping(value = "/{labelId}", method = RequestMethod.PUT)
+    public Result update(@RequestBody Label label, @PathVariable String labelId) {
+        label.setId(labelId);
+        labelService.update(label);
+        return new Result(true, StatusCode.OK, "修改成功");
+    }
+    @RequestMapping(value = "/{labelId}",method = RequestMethod.DELETE)
+    public Result deleteById(@PathVariable String labelId){
+        labelService.deleteById(labelId);
+        return new Result(true, StatusCode.OK, "删除成功");
+    }
+}

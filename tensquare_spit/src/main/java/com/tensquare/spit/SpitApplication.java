0528ab6b5f20a809a91e1274252608c22b36da6f
a2008q <a2008q@sina.com>
2021-09-05 16:44:52 +0800

    添加吐槽模块

---
+++ tensquare_spit/src/main/java/com/tensquare/spit/SpitApplication.java
@@ -0,0 +1,24 @@
+package com.tensquare.spit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+
+/**
+ * 应用模块名称<p>
+ * 代码描述<p>
+ *
+ * @author a2008q
+ * @since 2021/9/5 16:43
+ */
+@SpringBootApplication
+public class SpitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpitApplication.class, args);
+    }
+    @Bean
+    public IdWorker idWorkker(){
+        return new IdWorker(1, 1);
+    }
+}

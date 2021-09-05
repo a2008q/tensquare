c62d8b15fd7464c3237893a1d17cc6059b7f385f
a2008q <a2008q@sina.com>
2021-09-05 10:21:48 +0800

    完善最新回答、热门回答、等待回答列表的接口

--- tensquare_qa/src/main/java/com/tensquare/qa/dao/ProblemDao.java
+++ tensquare_qa/src/main/java/com/tensquare/qa/dao/ProblemDao.java
@@ -1,14 +1,45 @@
 package com.tensquare.qa.dao;
 
+import com.tensquare.qa.pojo.Problem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
-import com.tensquare.qa.pojo.Problem;
 /**
  * problem数据访问接口
- * @author Administrator
  *
+ * @author Administrator
  */
-public interface ProblemDao extends JpaRepository<Problem,String>,JpaSpecificationExecutor<Problem>{
-	
+public interface ProblemDao extends JpaRepository<Problem, String>, JpaSpecificationExecutor<Problem> {
+    /**
+     * 根据标签ID查询最新问题列表
+     *
+     * @param labelId  标签ID
+     * @param pageable 分页对象
+     * @return
+     */
+    @Query(value = "select * from tb_problem ,tb_pl where id=problemid and labelid =? ORDER BY replytime desc", nativeQuery = true)
+    Page<Problem> newList(String labelId, Pageable pageable);
+
+    /**
+     * 热门问答
+     *
+     * @param labelId  标签ID
+     * @param pageable 分页对象
+     * @return
+     */
+    @Query(value = "select * from tb_problem ,tb_pl where id=problemid and labelid =? ORDER BY reply desc", nativeQuery = true)
+    Page<Problem> hotList(String labelId, Pageable pageable);
+
+    /**
+     * 根据标签ID查询等待回答列表
+     *
+     * @param labelId  标签ID
+     * @param pageable 分页对象
+     * @return
+     */
+    @Query(value = "select * from tb_problem ,tb_pl where id=problemid and labelid =? AND reply=0  ORDER BY createtime desc", nativeQuery = true)
+    Page<Problem> waitList(String labelId, Pageable pageable);
 }
--- tensquare_qa/src/main/java/com/tensquare/qa/service/ProblemService.java
+++ tensquare_qa/src/main/java/com/tensquare/qa/service/ProblemService.java
@@ -1,157 +1,197 @@
 package com.tensquare.qa.service;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Expression;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import javax.persistence.criteria.Selection;
-
+import com.tensquare.qa.dao.ProblemDao;
+import com.tensquare.qa.pojo.Problem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-
 import util.IdWorker;
 
-import com.tensquare.qa.dao.ProblemDao;
-import com.tensquare.qa.pojo.Problem;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * problem服务层
- * 
- * @author Administrator
  *
+ * @author Administrator
  */
 @Service
 public class ProblemService {
 
-	@Autowired
-	private ProblemDao problemDao;
-	
-	@Autowired
-	private IdWorker idWorker;
+    @Autowired
+    private ProblemDao problemDao;
 
-	/**
-	 * 查询全部列表
-	 * @return
-	 */
-	public List<Problem> findAll() {
-		return problemDao.findAll();
-	}
+    @Autowired
+    private IdWorker idWorker;
 
-	
-	/**
-	 * 条件查询+分页
-	 * @param whereMap
-	 * @param page
-	 * @param size
-	 * @return
-	 */
-	public Page<Problem> findSearch(Map whereMap, int page, int size) {
-		Specification<Problem> specification = createSpecification(whereMap);
-		PageRequest pageRequest =  PageRequest.of(page-1, size);
-		return problemDao.findAll(specification, pageRequest);
-	}
+    /**
+     * 查询全部列表
+     *
+     * @return
+     */
+    public List<Problem> findAll() {
+        return problemDao.findAll();
+    }
 
-	
-	/**
-	 * 条件查询
-	 * @param whereMap
-	 * @return
-	 */
-	public List<Problem> findSearch(Map whereMap) {
-		Specification<Problem> specification = createSpecification(whereMap);
-		return problemDao.findAll(specification);
-	}
 
-	/**
-	 * 根据ID查询实体
-	 * @param id
-	 * @return
-	 */
-	public Problem findById(String id) {
-		return problemDao.findById(id).get();
-	}
+    /**
+     * 条件查询+分页
+     *
+     * @param whereMap
+     * @param page
+     * @param size
+     * @return
+     */
+    public Page<Problem> findSearch(Map whereMap, int page, int size) {
+        Specification<Problem> specification = createSpecification(whereMap);
+        PageRequest pageRequest = PageRequest.of(page - 1, size);
+        return problemDao.findAll(specification, pageRequest);
+    }
 
-	/**
-	 * 增加
-	 * @param problem
-	 */
-	public void add(Problem problem) {
-		// problem.setId( idWorker.nextId()+"" ); 雪花分布式ID生成器
-		problemDao.save(problem);
-	}
 
-	/**
-	 * 修改
-	 * @param problem
-	 */
-	public void update(Problem problem) {
-		problemDao.save(problem);
-	}
+    /**
+     * 条件查询
+     *
+     * @param whereMap
+     * @return
+     */
+    public List<Problem> findSearch(Map whereMap) {
+        Specification<Problem> specification = createSpecification(whereMap);
+        return problemDao.findAll(specification);
+    }
 
-	/**
-	 * 删除
-	 * @param id
-	 */
-	public void deleteById(String id) {
-		problemDao.deleteById(id);
-	}
+    /**
+     * 根据ID查询实体
+     *
+     * @param id
+     * @return
+     */
+    public Problem findById(String id) {
+        return problemDao.findById(id).get();
+    }
 
-	/**
-	 * 动态条件构建
-	 * @param searchMap
-	 * @return
-	 */
-	private Specification<Problem> createSpecification(Map searchMap) {
+    /**
+     * 增加
+     *
+     * @param problem
+     */
+    public void add(Problem problem) {
+        // problem.setId( idWorker.nextId()+"" ); 雪花分布式ID生成器
+        problemDao.save(problem);
+    }
 
-		return new Specification<Problem>() {
+    /**
+     * 修改
+     *
+     * @param problem
+     */
+    public void update(Problem problem) {
+        problemDao.save(problem);
+    }
 
-			@Override
-			public Predicate toPredicate(Root<Problem> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-				List<Predicate> predicateList = new ArrayList<Predicate>();
+    /**
+     * 删除
+     *
+     * @param id
+     */
+    public void deleteById(String id) {
+        problemDao.deleteById(id);
+    }
+
+    /**
+     * 动态条件构建
+     *
+     * @param searchMap
+     * @return
+     */
+    private Specification<Problem> createSpecification(Map searchMap) {
+
+        return new Specification<Problem>() {
+
+            @Override
+            public Predicate toPredicate(Root<Problem> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                List<Predicate> predicateList = new ArrayList<Predicate>();
                 // ID
-                if (searchMap.get("id")!=null && !"".equals(searchMap.get("id"))) {
-                	predicateList.add(cb.like(root.get("id").as(String.class), "%"+(String)searchMap.get("id")+"%"));
+                if (searchMap.get("id") != null && !"".equals(searchMap.get("id"))) {
+                    predicateList.add(cb.like(root.get("id").as(String.class), "%" + (String) searchMap.get("id") + "%"));
                 }
                 // 标题
-                if (searchMap.get("title")!=null && !"".equals(searchMap.get("title"))) {
-                	predicateList.add(cb.like(root.get("title").as(String.class), "%"+(String)searchMap.get("title")+"%"));
+                if (searchMap.get("title") != null && !"".equals(searchMap.get("title"))) {
+                    predicateList.add(cb.like(root.get("title").as(String.class), "%" + (String) searchMap.get("title") + "%"));
                 }
                 // 内容
-                if (searchMap.get("content")!=null && !"".equals(searchMap.get("content"))) {
-                	predicateList.add(cb.like(root.get("content").as(String.class), "%"+(String)searchMap.get("content")+"%"));
+                if (searchMap.get("content") != null && !"".equals(searchMap.get("content"))) {
+                    predicateList.add(cb.like(root.get("content").as(String.class), "%" + (String) searchMap.get("content") + "%"));
                 }
                 // 用户ID
-                if (searchMap.get("userid")!=null && !"".equals(searchMap.get("userid"))) {
-                	predicateList.add(cb.like(root.get("userid").as(String.class), "%"+(String)searchMap.get("userid")+"%"));
+                if (searchMap.get("userid") != null && !"".equals(searchMap.get("userid"))) {
+                    predicateList.add(cb.like(root.get("userid").as(String.class), "%" + (String) searchMap.get("userid") + "%"));
                 }
                 // 昵称
-                if (searchMap.get("nickname")!=null && !"".equals(searchMap.get("nickname"))) {
-                	predicateList.add(cb.like(root.get("nickname").as(String.class), "%"+(String)searchMap.get("nickname")+"%"));
+                if (searchMap.get("nickname") != null && !"".equals(searchMap.get("nickname"))) {
+                    predicateList.add(cb.like(root.get("nickname").as(String.class), "%" + (String) searchMap.get("nickname") + "%"));
                 }
                 // 是否解决
-                if (searchMap.get("solve")!=null && !"".equals(searchMap.get("solve"))) {
-                	predicateList.add(cb.like(root.get("solve").as(String.class), "%"+(String)searchMap.get("solve")+"%"));
+                if (searchMap.get("solve") != null && !"".equals(searchMap.get("solve"))) {
+                    predicateList.add(cb.like(root.get("solve").as(String.class), "%" + (String) searchMap.get("solve") + "%"));
                 }
                 // 回复人昵称
-                if (searchMap.get("replyname")!=null && !"".equals(searchMap.get("replyname"))) {
-                	predicateList.add(cb.like(root.get("replyname").as(String.class), "%"+(String)searchMap.get("replyname")+"%"));
+                if (searchMap.get("replyname") != null && !"".equals(searchMap.get("replyname"))) {
+                    predicateList.add(cb.like(root.get("replyname").as(String.class), "%" + (String) searchMap.get("replyname") + "%"));
                 }
-				
-				return cb.and( predicateList.toArray(new Predicate[predicateList.size()]));
 
-			}
-		};
+                return cb.and(predicateList.toArray(new Predicate[predicateList.size()]));
 
-	}
+            }
+        };
+
+    }
+
+    /**
+     * 根据标签ID查询问题列表
+     *
+     * @param labelId 标签ID
+     * @param page    页码
+     * @param rows    页大小
+     * @return
+     */
+    public Page<Problem> newList(String labelId, int page, int rows) {
+        Pageable pageable = PageRequest.of(page - 1, rows);
+        return problemDao.newList(labelId, pageable);
+    }
+
+    /**
+     * 热门问答
+     *
+     * @param labelid 标签ID
+     * @param page    页码
+     * @param rows    页大小
+     * @return
+     */
+    public Page<Problem> hotList(String labelid, int page, int rows) {
+        Pageable pageable = PageRequest.of(page - 1, rows);
+        return problemDao.hotList(labelid, pageable);
+    }
+
+    /**
+     * 根据标签ID查询等待回答列表
+     *
+     * @param labelId 标签ID
+     * @param page    页码
+     * @param rows    页大小
+     * @return
+     */
+    public Page<Problem> waitList(String labelId, int page, int rows) {
+        Pageable pageable = PageRequest.of(page - 1, rows);
+        return problemDao.waitList(labelId, pageable);
+    }
 
 }

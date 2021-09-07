8931b15370850a387919cd36027ba2daedb8cafe
a2008q <a2008q@sina.com>
2021-09-07 20:48:25 +0800

    将配置文件上传至github，使用SpringCloudConfig，SpringCloudBus管理配置文件

---
+++ tensquare_config/src/main/java/com/tensquare/config/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.tensquare.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@SpringBootApplication
+// 开启配置服务
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}

c1309a199ebdbfeb5bde34939844647f64b2e816
a2008q <a2008q@sina.com>
2021-09-07 16:01:47 +0800

    简单使用hystrix

--- tensquare_qa/src/main/java/com/tensquare/qa/client/LabelClient.java
+++ tensquare_qa/src/main/java/com/tensquare/qa/client/LabelClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("tensquare-base")
+@FeignClient(value = "tensquare-base")
 public interface LabelClient {
     @RequestMapping(value = "/label/{id}", method = RequestMethod.GET)
     Result findById(@PathVariable("id") String id);
